29dd59e5a6ff3d83ff496f4180821e1fc348f657
sidongdong <s0511eun@gmail.com>
2022-11-13 12:50:21 +0900

    git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='flowchartWrapper' style={{background: 'url(./assets/flowchart/flowTree.png)', backgroundSize: 'auto 100%', backgroundRepeat: 'no-repeat'}}>
+        <img id="declareFlow" alt="flow1" src="assets/flowchart/declareFlow.png"></img>
+        <img id="inputFlow" alt="flow2" src="assets/flowchart/inputFlow.png"></img>
+        <img id="processFlow" alt="flow3" src="assets/flowchart/processFlow.png"></img>
+        <img id="forFlow" alt="flow4" src="assets/flowchart/forFlow.png"></img>
+        <img id="ifFlow" alt="flow5" src="assets/flowchart/ifFlow.png"></img>
+        <img id="outputFlow" alt="flow6" src="assets/flowchart/outputFlow.png"></img>
+      </div>
+      <div className='flowCodeArea'>
+
+      </div>
     </div>
   );
 }
